51df981f04ba9ea655cac3bb5c2605727e884849
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-05-20 22:36:05 +0700

    String functions

--- string/string.js
+++ string/string.js
@@ -23,6 +23,7 @@ arr.includes('c');
 */
 
 // in ra cac so co trong mang a nhung khong co trong mang b
+/*
 let a = [9, 2, 11, 15];
 let b = [11, 3, 7, 9];
 
@@ -68,3 +69,57 @@ for (let itemA of a) {
 
 // using functional programming/lamda expression/arrow functions
 a.filter(item=> !b.includes(item)).forEach(item => console.log(item));
+*/
+
+let str = "Hello C04";
+
+//
+console.log(str.length);
+console.log(str[4]);
+console.log(str.at(4));
+
+console.log(str[-1]);
+console.log(str.at(-1));
+console.log(str.at(-3));
+
+// khong thay doi duoc
+str[0] = 'Z';
+console.log(str);
+
+// split
+let split = str.split(' ');
+console.log(split);
+
+let split1 = str.split('');
+console.log(split1);
+
+// let join1 = split1.join('');
+let join1 = split1.join('***');
+console.log(join1);
+
+// indexOf
+console.log(str.indexOf('l'));
+console.log(str.lastIndexOf('l'));
+console.log(str.lastIndexOf('llo'));
+console.log(str.lastIndexOf('llo'));
+console.log(str.lastIndexOf('Llo'));
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str.includes('04'));
+
+//
+let slice = str.slice();
+console.log(slice);
+
+let slice2 = str.slice(0, 5);
+console.log(slice2);
+
+console.log(str.slice(2, 8));
+
+
+
+
+
+
